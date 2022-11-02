07a66623c61ff434e3780d84c31a9868465ef1ce
AnhJenda <100700718+TinyTots-ePj@users.noreply.github.com>
2022-11-02 19:31:09 +0700

    commit

--- Day2/Lab01/WhatAge/server.js
+++ Day2/Lab01/WhatAge/server.js
@@ -10,5 +10,5 @@ app.use('/', function (req, res) {
 });
 
 app.listen(8081, function () {
-    console.log('Server WhatAge is running at http://127.0.0.1:8081/');
+    console.log('Server WhatAge is running at http://127.0.0.1:8081/' )
 });

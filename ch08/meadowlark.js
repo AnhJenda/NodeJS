c0820bea9b9da3f568b9f77ed6bbb7ee6c324776
AnhJenda <100700718+TinyTots-ePj@users.noreply.github.com>
2022-11-12 00:15:03 +0700

    commit

---
+++ ch08/meadowlark.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const expressHandlerbars = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.engine('hbs', expressHandlerbars.engine({
+    extname: '.handlebars',
+    defaultLayout: 'main'
+}))
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process',
+handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you',
+handlers.newsletterSignupThankYou)
+
+app.listen(3000, (req, res) => {
+    console.log('listening on port 3000')
+})
